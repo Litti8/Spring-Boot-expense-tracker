5a6dcc8a72c1a7d3285b4d83ea583f97e2b25e6b
Litti8 <lisandropiatti@gmail.com>
2025-09-22 16:22:35 +0200

    feat(auth): Create the JwtTokenProvider to generate tokens

---
+++ src/main/java/com/expensetracker/security/JwtTokenProvider.java
@@ -0,0 +1,40 @@
+package com.expensetracker.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
+    @Value("${jwt.expiration}")
+    private Long jwtExpirationInMs;
+
+    // Methods for generating and validating JWT tokens
+    public String generateToken(Authentication authentication){
+
+        String username = authentication.getName();
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + jwtExpirationInMs);
+
+        Key key = Keys.hmacShaKeyFor(jwtSecret.getBytes());
+
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(key, SignatureAlgorithm.HS512)
+                .compact();
+
+    }
+}
