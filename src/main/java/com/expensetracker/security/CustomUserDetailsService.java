f227a7170ee8916c4ea143dd08853d2924eba23a
Litti8 <lisandropiatti@gmail.com>
2025-09-23 11:58:34 +0200

    feat(auth): create CustomUserDetailService as a service to get user info from database

---
+++ src/main/java/com/expensetracker/security/CustomUserDetailsService.java
@@ -0,0 +1,44 @@
+package com.expensetracker.security;
+
+import com.expensetracker.entity.User;
+import com.expensetracker.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + email));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                mapRolesToAuthorities(user)
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> mapRolesToAuthorities(User user){
+
+        return Collections.singleton(new SimpleGrantedAuthority(
+                "ROLE_" + user.getRole().name()));
+    }
+}
